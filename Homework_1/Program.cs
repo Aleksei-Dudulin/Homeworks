d91e5270601a57ce4c52922766e57e7507fed743
Aleksei <dulittlefamily@yandex.ru>
2022-10-14 19:52:09 +0300

    Added task 2

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Hello, I can determine maximal and minimal numbers!");
+Console.Write("Write a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1, min = num2;
+if (num2 > max)
+{
+    max = num2;
+    min = num1;
+}
+Console.WriteLine($"Maximal number is {max}, minamal number is {min}");
